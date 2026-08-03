e7cb682ffef2593664586cd58ff091c77be0d742
Reparodynamics <papadenico@yahoo.com>
2026-08-03 14:47:53 -0600

    Load Phase 2 Nico art fixes

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import FullAppSync from "./FullAppSync";
 import NicoGuide from "./NicoGuide";
 import NicoWorldExperience from "./nico/NicoWorldExperience";
 import "./nico/nico-art-compat.css";
+import "./nico/nico-phase2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
