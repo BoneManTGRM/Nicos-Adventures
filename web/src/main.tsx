b877c57e7bf627a8339f72f0494a08b116334357
Reparodynamics <papadenico@yahoo.com>
2026-08-03 12:46:13 -0600

    Render Nico-enhanced platform experience

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import FullApp from "./FullApp";
+import NicoEnhancedApp from "./NicoEnhancedApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FullApp />
+    <NicoEnhancedApp />
   </StrictMode>
 );
