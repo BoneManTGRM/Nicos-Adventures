76bc5ff36a88328c5079404367e2428659760db8
Reparodynamics <papadenico@yahoo.com>
2026-08-03 14:19:52 -0600

    Mount synchronized FullApp boundary

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import FullApp from "./FullApp";
+import FullAppSync from "./FullAppSync";
 import NicoGuide from "./NicoGuide";
 import NicoWorldExperience from "./nico/NicoWorldExperience";
 import "./nico/nico-art-compat.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FullApp />
+    <FullAppSync />
     <NicoGuide />
     <NicoWorldExperience />
   </StrictMode>
