9dcb36605e0a6a72b9daf6dee9f22c3c508d7175
Reparodynamics <papadenico@yahoo.com>
2026-08-03 13:59:00 -0600

    Load Nico artwork compatibility styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import FullApp from "./FullApp";
 import NicoGuide from "./NicoGuide";
 import NicoWorldExperience from "./nico/NicoWorldExperience";
+import "./nico/nico-art-compat.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
