c3d8a240f1332bb41b0d93332182610c24fe5481
Reparodynamics <papadenico@yahoo.com>
2026-08-03 13:56:52 -0600

    Mount Nico Clubhouse and Showtime experience

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import FullApp from "./FullApp";
 import NicoGuide from "./NicoGuide";
+import NicoWorldExperience from "./nico/NicoWorldExperience";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <FullApp />
     <NicoGuide />
+    <NicoWorldExperience />
   </StrictMode>
 );
