3e7fca9e7b73df9d1579f252adee02021cd4031d
Reparodynamics <papadenico@yahoo.com>
2026-08-03 20:27:06 -0600

    Load Nico system stabilization styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -10,6 +10,7 @@ import "./nico/nico-phase2.css";
 import "./nico/approved-nico-art.css";
 import "./nico/nico-drag-studio.css";
 import "./nico/nico-about.css";
+import "./nico/system-stabilization.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
