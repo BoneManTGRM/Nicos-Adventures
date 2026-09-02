6907ee0c1cda7780031a9ff7e84c25433abca6b2
Reparodynamics <papadenico@yahoo.com>
2026-09-02 07:29:53 -0600

    Load final approved Monster Lab finish layer

--- web/src/world/MonsterLabVisuals.tsx
+++ web/src/world/MonsterLabVisuals.tsx
@@ -1,6 +1,7 @@
 import "./monster-lab-approved-layout.css";
 import "./monster-movement-poses.css";
 import "./monster-portrait.css";
+import "./monster-lab-target-fixes.css";
 
 export default function MonsterLabVisuals() {
   return null;
