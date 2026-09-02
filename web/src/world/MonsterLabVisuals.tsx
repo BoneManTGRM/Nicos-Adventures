9ddf0a551d1868675d32d8656187bc430a374789
Reparodynamics <papadenico@yahoo.com>
2026-09-02 06:27:31 -0600

    Code-split Monster Lab visual styling

---
+++ web/src/world/MonsterLabVisuals.tsx
@@ -0,0 +1,8 @@
+import "./monster-lab-approved-layout.css";
+import "./monster-movement-poses.css";
+import "./monster-movement-runtime.css";
+import "./monster-portrait.css";
+
+export default function MonsterLabVisuals() {
+  return null;
+}
