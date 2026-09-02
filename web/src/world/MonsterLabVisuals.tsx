0af7734bbe0f69c5176e938806ff2c7483a4cd46
Reparodynamics <papadenico@yahoo.com>
2026-09-02 06:31:10 -0600

    Load the compact Monster Lab visual bundle

--- web/src/world/MonsterLabVisuals.tsx
+++ web/src/world/MonsterLabVisuals.tsx
@@ -1,6 +1,5 @@
 import "./monster-lab-approved-layout.css";
 import "./monster-movement-poses.css";
-import "./monster-movement-runtime.css";
 import "./monster-portrait.css";
 
 export default function MonsterLabVisuals() {
